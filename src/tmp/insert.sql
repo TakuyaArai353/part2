e2d82ae235cb1a8521e58ea707f00ec4caea45ca
TakuyaArai353 <bonsunsogo@gmail.com>
2022-10-24 23:39:08 +0900

    add:insertでdate型のカラムにVARCHAR型のデータを登録し、挙動を確認:eyes:

--- src/tmp/insert.sql
+++ src/tmp/insert.sql
@@ -7,3 +7,22 @@ INSERT INTO companies (
     '2013-02-01',
     'Shintaro Yamada'
 );
+
+INSERT INTO companies (
+    name,
+    establishment_date,
+    founder
+) VALUES (
+    'yumemi inc',
+    '2000-01-27',
+    'Tosiyuki Kataoka'
+);
+INSERT INTO companies (
+    name,
+    establishment_date,
+    founder
+) VALUES (
+    'yumemi inc',
+    'sadfe',
+    'Tosiyuki Kataoka'
+);
