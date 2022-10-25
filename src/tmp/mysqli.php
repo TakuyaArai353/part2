621dae7899f7de4f96f3d1e0789b0dbf29f5f0b9
TakuyaArai353 <bonsunsogo@gmail.com>
2022-10-25 20:15:57 +0900

    add:PHPからMySQLにデータを追加

--- src/tmp/mysqli.php
+++ src/tmp/mysqli.php
@@ -10,5 +10,26 @@ if (!$link) {
 
 echo 'データベースに接続できました' . PHP_EOL;
 
+$sql = <<<EOT
+INSERT INTO companies (
+    name,
+    establishment_date,
+    founder
+) VALUES (
+    'SmartHR Inc',
+    '2013-01-23',
+    'Syouji Miyata'
+)
+EOT;
+
+$result = mysqli_query($link, $sql);
+
+if ($result) {
+    echo 'データを追加しました' . PHP_EOL;
+} else {
+    echo 'Error: データの追加に失敗しました' . PHP_EOL;
+    echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+}
+
 mysqli_close($link);
 echo 'データベーストの接続を切断しました' . PHP_EOL;
