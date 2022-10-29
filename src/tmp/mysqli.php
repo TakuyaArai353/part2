f01a06e84a677255b2b7be70d364b1551a459cfb
TakuyaArai353 <bonsunsogo@gmail.com>
2022-10-29 21:23:43 +0900

    add:PHPからMySQLのデータを取得
    メモリの解放を忘れずに

--- src/tmp/mysqli.php
+++ src/tmp/mysqli.php
@@ -10,26 +10,40 @@ if (!$link) {
 
 echo 'データベースに接続できました' . PHP_EOL;
 
-$sql = <<<EOT
-INSERT INTO companies (
-    name,
-    establishment_date,
-    founder
-) VALUES (
-    'SmartHR Inc',
-    '2013-01-23',
-    'Syouji Miyata'
-)
-EOT;
+// データの取得
+$sql = 'SELECT name FROM companies';
 
 $result = mysqli_query($link, $sql);
 
-if ($result) {
-    echo 'データを追加しました' . PHP_EOL;
-} else {
-    echo 'Error: データの追加に失敗しました' . PHP_EOL;
-    echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+// 連想配列で結果を取得
+while ($company = mysqli_fetch_assoc($result)) {
+    echo '会社名：' . $company['name'] . PHP_EOL;
 }
+
+// メモリの解放
+mysqli_free_result(($result));
+
+// データの追加
+// $sql = <<<EOT
+// INSERT INTO companies (
+//     name,
+//     establishment_date,
+//     founder
+// ) VALUES (
+//     'SmartHR Inc',
+//     '2013-01-23',
+//     'Syouji Miyata'
+// )
+// EOT;
+
+// $result = mysqli_query($link, $sql);
+
+// if ($result) {
+//     echo 'データを追加しました' . PHP_EOL;
+// } else {
+//     echo 'Error: データの追加に失敗しました' . PHP_EOL;
+//     echo 'Debugging error: ' . mysqli_error($link) . PHP_EOL;
+// }
 
 mysqli_close($link);
 echo 'データベーストの接続を切断しました' . PHP_EOL;
