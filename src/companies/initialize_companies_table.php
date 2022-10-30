21ea7bcdb6c299d7ecc208c17f3b7993cbde7093
TakuyaArai353 <bonsunsogo@gmail.com>
2022-10-30 11:15:48 +0900

    add: PHPからテーブルを作成:hatching_chick:

---
+++ src/companies/initialize_companies_table.php
@@ -0,0 +1,52 @@
+<?php
+
+function dbConnect()
+{
+    $link = mysqli_connect('db', 'book_log', 'pass', 'book_log');
+    if (!$link) {
+        echo 'Error: データベースに接続できません' . PHP_EOL;
+        echo 'Debugging error: : ' . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    return $link;
+}
+
+function dropTable($link)
+{
+    $dropTableSql = 'DROP TABLE IF EXISTS companies;';
+    $result = mysqli_query($link, $dropTableSql);
+    if ($result) {
+        echo 'テーブルを削除しました' . PHP_EOL .PHP_EOL;
+    } else {
+        echo 'Error: テーブルの削除に失敗しました' . PHP_EOL;
+        echo 'Debugging error: : ' . mysqli_error($link) . PHP_EOL;
+        exit;
+    }
+}
+
+function createTable($link)
+{
+    $createTableSql = <<<EOT
+    CREATE TABLE companies (
+        id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        name VARCHAR(255),
+        establishment_date DATE,
+        founder VARCHAR(255),
+        create_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ) DEFAULT CHARACTER SET=utf8mb4;
+    EOT;
+
+    $result = mysqli_query($link, $createTableSql);
+    if ($result) {
+        echo 'テーブルを作成しました' . PHP_EOL .PHP_EOL;
+    } else {
+        echo 'Error: テーブルの作成に失敗しました' . PHP_EOL;
+        echo 'Debugging error: : ' . mysqli_error($link) . PHP_EOL;
+        exit;
+    }
+}
+
+$link = dbConnect();
+dropTable($link);
+createTable($link);
+mysqli_close($link);
