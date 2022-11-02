aa151a27c229bff55f3c850c6d0cbcb174033c5d
TakuyaArai353 <bonsunsogo@gmail.com>
2022-11-02 20:14:33 +0900

    add: 読書ログ登録ページを表示

---
+++ src/new.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="wedth=device-width,initial-scale=1">
+
+    <title>読書ログ登録</title>
+</head>
+<body>
+    <p>Hello, world!</p>
+</body>
+</html>
