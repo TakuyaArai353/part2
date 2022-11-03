55e2eb92f00cea535754196b174640fa39ffe5fe
TakuyaArai353 <bonsunsogo@gmail.com>
2022-11-03 20:29:03 +0900

    add: フォームからデータベースにデータを登録する処理を追加

--- src/companies/create.php
+++ src/companies/create.php
@@ -2,6 +2,26 @@
 
 require_once __DIR__ . '/lib/mysqli.php';
 
+function createCompany($link, $company)
+{
+    $sql = <<<EOT
+    INSERT INTO companies (
+        name,
+        establishment_date,
+        founder
+    ) VALUES (
+        "{$company['name']}",
+        "{$company['establishment_date']}",
+        "{$company['founder']}"
+    )
+    EOT;
+    $result = mysqli_query($link, $sql);
+
+    if ($result) {
+        echo '登録が完了しました' . PHP_EOL;
+    }
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $company = [
         'name' => $_POST['name'],
@@ -12,9 +32,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // バリデーションする
     $link = dbConnect();
 
-    // データベスにデータを登録する
     createCompany($link, $company);
-    // データベースとの接続を切断する
+
+    mysqli_close($link);
 }
 
 ?>
