a893544a2cc64d711c38d27897c5a3b1bbd8deb4
TakuyaArai353 <bonsunsogo@gmail.com>
2022-11-06 17:39:16 +0900

    refactor: 共通のHTMLを一つにまとめる

--- src/companies/create.php
+++ src/companies/create.php
@@ -58,7 +58,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'founder' => $_POST['founder']
     ];
 
-    // バリデーションする
     $errors = validate($company);
 
     if (!count($errors)) {
@@ -70,39 +69,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 }
 
-
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-
-    <title>会社情報の登録</title>
-</head>
-<body>
-    <h1>会社情報の登録</h1>
-    <form action="create.php" method="POST">
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) :?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach;?>
-            </ul>
-        <?php endif; ?>
-
-        <div>
-            <label for="name">会社名</label>
-            <input type="text" id="name" name="name">
-        </div>
-        <div>
-            <label for="establishment_date">設立日</label>
-            <input type="date" id="establishment_date" name="establishment_date">
-        </div>
-        <div>
-            <label for="founder">代表者</label>
-            <input type="text" id="founder" name="founder">
-        </div>
-        <button type="submit">登録する</button>
-</body>
-</html>
+include 'views/new.php';
--- src/companies/new.php
+++ src/companies/new.php
@@ -1,14 +1,4 @@
 <?php
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+$errors = [];
 
-    <title>会社情報の登録</title>
-</head>
-<body>
-    <p>shs</p>
-</body>
-</html>
+include 'views/new.php';
